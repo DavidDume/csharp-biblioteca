3be432561699f80f4a5b8b84f06b47f39cf9d99b
David Dume <dumedavid11@gmail.com>
2023-09-11 15:07:48 +0200

    Add project files.

---
+++ csharp-biblioteca/Documento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Documento
+    {
+        public string Codice { get; } = "DOC" + Random.Shared.Next(0, 999);
+        public string Titolo { get; set; }
+        public int Anno { get; set; }
+        public string Settore { get; set; }
+        public string PosizioneScaffale { get; set; }
+        public string Autore { get; set; }
+
+        public Documento(string titolo, int anno, string settore, string posizioneScaffale, string Autore) { 
+            this.Anno = anno;
+            this.Settore = settore;
+            this.Titolo = titolo;
+            this.PosizioneScaffale = posizioneScaffale;
+            this.Autore = Autore;
+        }
+    }
+}
