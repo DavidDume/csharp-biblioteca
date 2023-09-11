1db1efc46772870b9a95d7061477f3db49aa05d3
David Dume <dumedavid11@gmail.com>
2023-09-11 15:27:04 +0200

    creazione classe utente

---
+++ csharp-biblioteca/Utente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Utente
+    {
+        public string Cognome { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string Telefono { get; private set; }
+
+        public Utente(string cognome, string nome, string email, string password, string telefono)
+        {
+            Cognome = cognome;
+            Nome = nome;
+            Email = email;
+            Password = password;
+            Telefono = telefono;
+        }
+    }
+}
